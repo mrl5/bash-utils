14e89abc939b906f63ec943850d7923a2e4f177c
mrl5 <31549762+mrl5@users.noreply.github.com>
2018-05-30 14:29:03 +0200

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Copyright 2018 mrl5
+# Distributed under the terms of the GNU General Public License v3
+
+STARTDIR=$(pwd -P)
+BACKUPTARGET=$1
+BACKUPARCHIVE=$(basename $BACKUPTARGET)-$(date +%Y%m%d).tar.bz2
+# "$(dirname $0)" - script location
+cd $(dirname $0)/GitRepositories-mail
+BACKUPPRIMARYDIR=$2
+BACKUPREDUNDANTDIR=$3
+BACKUPFAILMSG="Operation failed. Aborting."
+STARTDIR=$(pwd)
+
+# todo
+## if $2 and $3 not given then ...
+
+echo "Creating backup..."
+cd $BACKUPTARGET
+tar --exclude=new-cls.git -jcvf $BACKUPPRIMARYDIR/$BACKUPARCHIVE *
+if [ $? -eq 0 ]; then
+    echo "Created" $BACKUPARCHIVE "in" $BACKUPPRIMARYDIR
+    cd $BACKUPPRIMARYDIR
+    echo "Copying backup to another directory..."
+    cp -v $BACKUPARCHIVE $BACKUPREDUNDANTDIR
+    if [ $? -eq 0 ]; then
+        echo "Copied" $BACKUPARCHIVE to $BACKUPREDUNDANTDIR
+    else
+        echo $BACKUPFAILMSG
+    fi
+else
+    echo $BACKUPFAILMSG
+fi
+cd $STARTDIR
