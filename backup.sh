d8a740a67e777d567e5d90743addef8443f7ce0b
mrl5 <jakuboo+github@gmail.com>
2018-05-31 12:18:30 +0200

    feature: rename archive if targetdir is hidden dir

--- backup.sh
+++ backup.sh
@@ -2,13 +2,24 @@
 # Copyright 2018 mrl5
 # Distributed under the terms of the GNU General Public License v3
 
-# Example 1: 'sh backup.sh /path/to/target' (creates backup.tar.bz2 in current directory)
-# Example 2: 'sh backup.sh /path/to/target /path/to/backup/directory' (creates backup.tar.bz2 in given directory)
+# This script creates backup of content of given directory
+# Example 1: 'sh backup.sh /path/to/target/directory'
+#  creates "directory-YYMMDD.tar.bz2" archive in current directory
+# Example 2: 'sh backup.sh /path/to/target/directory /path/to/backup/directory'
+#  creates "directory-YYMMDD.tar.bz2" archive in given backup directory
 
 STARTDIR=$(pwd -P)
 BACKUPTARGET=$1
-BACKUPARCHIVE=$(basename $BACKUPTARGET)-$(date +%Y%m%d).tar.bz2
 BACKUPFAILMSG="Operation failed. Aborting."
+
+#if directory starts with only one "." ...
+if [[ $(basename $BACKUPTARGET) =~ ^\.[^.]. ]]; then
+  PARTIALNAME=$(basename $BACKUPTARGET | cut -d'.' -f2-)
+else
+  PARTIALNAME=$(basename $BACKUPTARGET)
+fi
+
+BACKUPARCHIVE=$PARTIALNAME-$(date +%Y%m%d).tar.bz2
 
 #if length of $2 is zero
 if [ -z $2 ]; then
@@ -19,7 +30,7 @@ fi
 
 echo "Creating backup..."
 cd $BACKUPTARGET
-tar -jcvf $BACKUPDIR/$BACKUPARCHIVE *
+tar -jcvf $BACKUPDIR/$BACKUPARCHIVE .
 #$? success flag of last operation (0 = success; other = fail)
 if [ $? -eq 0 ]; then
   echo "Created" $BACKUPARCHIVE "in" $BACKUPDIR
